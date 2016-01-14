3f60718e177c984c38562101392e17dd2eb25270
zach georgii <zgeorgii@gmail.com>
2016-01-13 22:14:15 -0800

    started css

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-//'use strict'
 
 var hours = ['6:00am','7:00am','8:00am','9:00am','10:00am','11:00am','12:00pm','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','6:00pm','7:00pm','8:00pm'];
 
@@ -9,10 +8,8 @@ function Coffeestand(stand, minCust, maxCust, cupsCust, lbsCust) {
   this.hourlyCust = [];
   this.cupsCust = cupsCust;
   this.hourlyCups = [];
-  //this.dailyCups = 0;
   this.lbsCust = lbsCust;
   this.hourlyLbs = [];
-  //this.dailyLbs = 0;
   this.hourlyCupLbs = [];
   this.allHourlyBeans = this.hourlyCupLbs + this.hourlyLbs;
   this.allDailyBeans = 0;
@@ -61,14 +58,85 @@ for (var i = 0; i < hours.length; i++){
 };
 
 var pike = new Coffeestand('Pike Place', 14, 55, 1.2, 3.7);
-pike.render();
+
 var capHill = new Coffeestand('Capitol Hill', 32, 48, 3.2, 0.4);
-capHill.render();
+
 var library = new Coffeestand('Seattle Public Library', 49, 75, 2.6, 0.2);
-library.render();
+
 var union = new Coffeestand('South Lake Union', 35, 88, 1.3, 3.7);
-union.render();
-var airport = new Coffeestand('Sea-Tac Airport', 124, 1.1, 2.7);
-airport.render();
+
+var airport = new Coffeestand('Sea-Tac Airport', 68, 124, 1.1, 2.7);
+
 var web = new Coffeestand('Website Sales', 3, 6, 0, 6.7);
-web.render();
+
+  pike.render();
+  capHill.render();
+  library.render();
+  union.render();
+  airport.render();
+  web.render();
+
+  // document.write('table');
+  // document.write('<tr><th>Coffee Stands</th></tr>');
+  // document.write('<tr><td>' + pike.render() + '</td><tr>');
+  // document.write('</table>')
+
+
+// var stands = ['pike.render()', 'capHill.render()', 'library.render()', 'union.render()', 'airport.render()', 'web.render()'];
+//
+//   document.write('<table>');
+//   document.write('<tr><th>Coffee Stands</th></tr>');
+//
+//   for(var i = 0; i < stands.length; i++){
+//     document.write('<tr><td>' + stands[i] + '</td></tr>');
+//   }
+//
+//   document.write('</table>');
+
+
+
+  // var tblEl = document.createElement('table');
+  //
+  // var trEl = document.createElement('tr');
+  //
+  // var thEl = document.createElement('th');
+  //
+  // thEl.textContent = 'Coffee Stands';
+  //
+  // trEl.appendChild(thEl);
+  //
+  // var tdEl = document.createElement('td');
+  // tdEl.textContent =
+  // trEl.appendChild(tdEl);
+  //
+  // tblEl.appendChild(trEl);
+
+// function makeTbl() {
+//
+//   var stands = [pike.render(), capHill.render(), library.render(), union.render(), airport.render(), web.render()];
+//
+//   var sectEl = document.getElementById('tweaks_finest');
+//
+//   var tblEl = document.createElement('table');
+//
+//   var trEl = document.createElement('tr');
+//
+//   var thEl = document.createElement('th');
+//
+//   thEl.textContent = 'Coffee Stands';
+//
+//   trEl.appendChild(thEl);
+//
+//   for (var i = 0; i < stands.length; i++) {
+//
+//   var tdEl = document.createElement('td');
+//
+//   tdEl.textContent = stands[i];
+//
+//   trEl.appendChild(tdEl);
+// }
+//   tblEl.appendChild(trEl);
+//
+//   sectEl.appendChild(tblEl);
+// }
+// makeTbl();
