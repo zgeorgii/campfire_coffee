3571bfe24df6765b33702528721915f5b1aa0111
zach georgii <zgeorgii@gmail.com>
2016-01-17 16:12:44 -0800

    added css and buttons

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+'use strict';
 
 var hours = ['6:00am','7:00am','8:00am','9:00am','10:00am','11:00am','12:00pm','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','6:00pm','7:00pm','8:00pm'];
 
@@ -13,16 +14,13 @@ function Coffeestand(stand, minCust, maxCust, cupsCust, lbsCust) {
   this.hourlyCupLbs = [];
   this.allHourlyBeans = this.hourlyCupLbs + this.hourlyLbs;
   this.allDailyBeans = 0;
-
+  // stand.push(this);
 };
-
-
  Coffeestand.prototype.hourlyCustomers = function() {
   for (var i = 0; i < hours.length; i++) {
-this.hourlyCust.push(Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust));
+this.hourlyCust.push(Math.ceil(Math.random() * (this.maxCust - this.minCust) + this.minCust));
   }
 };
-
 Coffeestand.prototype.hourlyCupsEqu = function() {
   for (var i = 0; i < hours.length; i++) {
     var cups = this.cupsCust * this.hourlyCust[i];
@@ -31,32 +29,22 @@ Coffeestand.prototype.hourlyCupsEqu = function() {
     this.allDailyBeans += (cups / 20);
   }
 };
-
 Coffeestand.prototype.hourlyLbsEqu = function() {
   for (var i = 0; i < hours.length; i++) {
-    var beans = Math.floor(this.lbsCust * this.hourlyCust[i]);
+    var beans = (this.lbsCust * this.hourlyCust[i]);
     this.hourlyLbs.push(beans);
     this.allDailyBeans += beans;
   }
 };
-
 Coffeestand.prototype.render = function() {
   var sectEl = document.getElementById('tweaks_finest');
-
   var tblEl = document.createElement('table');
-
   var trEl = document.createElement('tr');
-
   var thEl = document.createElement('th');
-
   thEl.textContent = this.stand;
-
   trEl.appendChild(thEl);
-
   tblEl.appendChild(trEl);
-
   sectEl.appendChild(tblEl);
-
   var trEl2 = document.createElement('tr');
   var thEl2 = document.createElement('th');
 
@@ -69,41 +57,22 @@ Coffeestand.prototype.render = function() {
     tdEl.textContent = this.hourlyLbs[i].toFixed[1];
     trEl2.appendChild(tdEl);
   }
-
   for (var i = 0; i < hours.length; i++){
-    var tdEl = document.createElement('td');
-    tdEl.textContent = hours[i] + ": " + (this.hourlyCupLbs[i] + this.hourlyLbs[i]).toFixed(1) +  ' lbs ' + this.hourlyCust[i] + ' customers, ' + this.hourlyCups[i].toFixed(1) + 'cups (' + this.hourlyCupLbs[i].toFixed(1) + ' lbs), ' + this.hourlyLbs[i].toFixed(1) + 'lbs to-go';
-    trEl.appendChild(tdEl);
-    }
-
-    tdEl = document.createElement('td');
-      tdEl.textContent = 'Total lbs of Beans Sold at this Location: ' + this.allDailyBeans.toFixed(1);
-      trEl.appendChild(tdEl);
-
-// var ulEl = document.createElement('ul');
-// ulEl.appendChild(document.createTextNode(this.stand));
-// var sectionEl = document.getElementById('tweaks_finest').appendChild(ulEl);
-//
-// for (var i = 0; i < hours.length; i++){
-//   var liEl = document.createElement('li');
-//   liEl.textContent = hours[i] + ": " + (this.hourlyCupLbs[i] + this.hourlyLbs[i]).toFixed(1) +  ' lbs [' + this.hourlyCust[i] + ' customers, ' + this.hourlyCups[i].toFixed(1) + 'cups (' + this.hourlyCupLbs[i].toFixed(1) + ' lbs), ' + this.hourlyLbs[i].toFixed(1) + 'lbs to-go]';
-//   ulEl.appendChild(liEl);
-//   }
-//   liEl = document.createElement('li');
-//   liEl.textContent = 'Total lbs of Beans Sold at this Location: ' + this.allDailyBeans.toFixed(1);
-//   ulEl.appendChild(liEl);
+    var tdEl2 = document.createElement('td');
+    var shorten = (this.hourlyCupLbs[i] + this.hourlyLbs[i]).toFixed(1);
+    tdEl2.textContent = hours[i] + ": " + shorten +  " lbs " + this.hourlyCust[i] + " customers, " + this.hourlyCups[i].toFixed(1) + "cups (" + this.hourlyCupLbs[i].toFixed(1) + " lbs)," + this.hourlyLbs[i].toFixed(1) + 'lbs to-go';
+    trEl.appendChild(tdEl2);
+  }
+  var tdEl2 = document.createElement('td');
+      tdEl2.textContent = 'Total lbs of Beans Sold at this Location: ' + this.allDailyBeans.toFixed(1);
+      trEl.appendChild(tdEl2);
 };
-
+// var newLocation = new Coffeestand(stand, +minCust, +maxCust, +cupsCust, +lbsCust);
 var pike = new Coffeestand('Pike Place', 14, 55, 1.2, 3.7);
-
 var capHill = new Coffeestand('Capitol Hill', 32, 48, 3.2, 0.4);
-
 var library = new Coffeestand('Seattle Public Library', 49, 75, 2.6, 0.2);
-
 var union = new Coffeestand('South Lake Union', 35, 88, 1.3, 3.7);
-
 var airport = new Coffeestand('Sea-Tac Airport', 68, 124, 1.1, 2.7);
-
 var web = new Coffeestand('Website Sales', 3, 6, 0, 6.7);
 
   pike.render();
@@ -112,39 +81,3 @@ var web = new Coffeestand('Website Sales', 3, 6, 0, 6.7);
   union.render();
   airport.render();
   web.render();
-  // document.write('table');
-  // document.write('<tr><th>Coffee Stands</th></tr>');
-  // document.write('<tr><td>' + pike.render() + '</td><tr>');
-  // document.write('</table>')
-
-
-// var stands = ['pike.render()', 'capHill.render()', 'library.render()', 'union.render()', 'airport.render()', 'web.render()'];
-//
-//   document.write('<table>');
-//   document.write('<tr><th>Coffee Stands</th></tr>');
-//
-//   for(var i = 0; i < stands.length; i++){
-//     document.write('<tr><td>' + stands[i] + '</td></tr>');
-//   }
-//
-//   document.write('</table>');
-
-
-
-
-
-  // var tblEl = document.createElement('table');
-  //
-  // var trEl = document.createElement('tr');
-  //
-  // var thEl = document.createElement('th');
-  //
-  // thEl.textContent = 'Coffee Stands';
-  //
-  // trEl.appendChild(thEl);
-  //
-  // var tdEl = document.createElement('td');
-  // tdEl.textContent =
-  // trEl.appendChild(tdEl);
-  //
-  // tblEl.appendChild(trEl);
